51b84e06665449c22cb16f5e75ff5711ae7a6938
Nurzhigit <yerassyl@sprintsquads.com>
2021-03-02 12:47:33 +0600

    Initial Commit

---
+++ AdaptiveFonts/AdaptiveFonts.h
@@ -0,0 +1,19 @@
+//
+//  AdaptiveFonts.h
+//  AdaptiveFonts
+//
+//  Created by Nurzhigit on 02.03.2021.
+//  Copyright © 2021 Sprint Squads. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdaptiveFonts.
+FOUNDATION_EXPORT double AdaptiveFontsVersionNumber;
+
+//! Project version string for AdaptiveFonts.
+FOUNDATION_EXPORT const unsigned char AdaptiveFontsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdaptiveFonts/PublicHeader.h>
+
+
